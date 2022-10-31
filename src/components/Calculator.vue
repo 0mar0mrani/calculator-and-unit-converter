9b192d44597e4afb9257f7814e0a02e3d5e58ccd
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2022-10-31 20:17:29 +0100

    Added more buttons to calculator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,6 +1,28 @@
 <template>
 	<section>
-		<button @click="calculate">=</button>
+		<div class="calculator__display">{{ display }}</div>
+
+		<button @click="displayNumberOperator($event)">1</button>
+		<button @click="displayNumberOperator($event)">2</button>
+		<button @click="displayNumberOperator($event)">3</button>
+		<button @click="displayNumberOperator($event)">4</button>
+		<button @click="displayNumberOperator($event)">5</button>
+		<button @click="displayNumberOperator($event)">6</button>
+		<button @click="displayNumberOperator($event)">7</button>
+		<button @click="displayNumberOperator($event)">8</button>
+		<button @click="displayNumberOperator($event)">9</button>
+		<button @click="displayNumberOperator($event)">0</button>
+
+		<button @click="displayNumberOperator($event)">.</button>
+
+		<button @click="displayNumberOperator($event)">+</button>
+		<button @click="displayNumberOperator($event)">-</button>
+		<button @click="displayNumberOperator($event)">*</button>
+		<button @click="displayNumberOperator($event)">/</button>
+
+		<button @click="resetCalculator">C</button>
+		
+		<button @click="calculateEquation">=</button>
 	</section>
 </template>
 
