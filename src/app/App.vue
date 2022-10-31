72d0c9b554d04e5b86b644651cd6a90b6464a130
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2022-10-31 19:21:03 +0100

    Calculation function where it takes an array

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,7 +1,15 @@
 <template>
+	<Calculator/>
 </template>
 
 <script>
+	import Calculator from '../components/Calculator.vue'
+
+	export default {
+		components: {
+			Calculator,
+		}
+	}
 </script>
 
 <style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,26 @@
+<template>
+	<section>
+		<button @click="calculate">=</button>
+	</section>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				values: ['78', '+', '5', '*','6']
+			}
+		},
+
+		methods: {
+			calculate() {
+				const calculationString = this.values.reduce((totalValue, oneInput) => totalValue + oneInput);
+				const answer = eval(calculationString);
+				console.log(answer);
+				return answer;
+			}
+		}
+	}
+</script>
+
+<style></style>
