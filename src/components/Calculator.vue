515853e1b2dcf82379428eca84a72536b0c4be10
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2022-10-31 20:34:35 +0100

    Styled calculator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,28 +1,33 @@
 <template>
-	<section>
+	<section class="calculator">
 		<div class="calculator__display">{{ display }}</div>
 
-		<button @click="displayNumberOperator($event)">1</button>
-		<button @click="displayNumberOperator($event)">2</button>
-		<button @click="displayNumberOperator($event)">3</button>
-		<button @click="displayNumberOperator($event)">4</button>
-		<button @click="displayNumberOperator($event)">5</button>
-		<button @click="displayNumberOperator($event)">6</button>
-		<button @click="displayNumberOperator($event)">7</button>
-		<button @click="displayNumberOperator($event)">8</button>
-		<button @click="displayNumberOperator($event)">9</button>
-		<button @click="displayNumberOperator($event)">0</button>
-
-		<button @click="displayNumberOperator($event)">.</button>
-
-		<button @click="displayNumberOperator($event)">+</button>
-		<button @click="displayNumberOperator($event)">-</button>
-		<button @click="displayNumberOperator($event)">*</button>
-		<button @click="displayNumberOperator($event)">/</button>
-
-		<button @click="resetCalculator">C</button>
-		
-		<button @click="calculateEquation">=</button>
+		<div class="calculator__buttons">
+			<div class="calculator__numbers">
+				<button @click="displayNumberOperator($event)" class="calculator__number">1</button>
+				<button @click="displayNumberOperator($event)" class="calculator__number">2</button>
+				<button @click="displayNumberOperator($event)" class="calculator__number">3</button>
+				<button @click="displayNumberOperator($event)" class="calculator__number">4</button>
+				<button @click="displayNumberOperator($event)" class="calculator__number">5</button>
+				<button @click="displayNumberOperator($event)" class="calculator__number">6</button>
+				<button @click="displayNumberOperator($event)" class="calculator__number">7</button>
+				<button @click="displayNumberOperator($event)" class="calculator__number">8</button>
+				<button @click="displayNumberOperator($event)" class="calculator__number">9</button>
+				<button @click="displayNumberOperator($event)" class="calculator__number">0</button>
+			</div>
+				
+			
+			<div class="calculator__operators">
+				<button @click="displayNumberOperator($event)" class="calculator__operator">+</button>
+				<button @click="displayNumberOperator($event)" class="calculator__operator">-</button>
+				<button @click="displayNumberOperator($event)" class="calculator__operator">*</button>
+				<button @click="displayNumberOperator($event)" class="calculator__operator">/</button>
+			</div>
+			
+			<button @click="displayNumberOperator($event)" class="calculator__period">.</button>
+			<button @click="resetCalculator" class="calculator__reset">C</button>
+			<button @click="calculateEquation" class="calculator__equals">=</button>
+		</div>
 	</section>
 </template>
 
@@ -55,4 +60,25 @@
 	}
 </script>
 
-<style></style>
+<style>
+.calculator {
+	display: flex;
+	flex-direction: column;
+}
+.calculator__display {
+	width: 100%;
+	height: 6rem;
+	font-size: 5rem;
+	background-color: antiquewhite;
+}
+
+.calculator__number,
+.calculator__period,
+.calculator__operator,
+.calculator__reset,
+.calculator__equals {
+	font-size: 2rem;
+	width: 4rem;
+	height: 4rem;
+}
+</style>
