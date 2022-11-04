cbcb22cd3876ece763756118957323c589deae39
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2022-11-04 09:54:09 +0100

    Created an component of weight and length component

---
+++ src/components/UnitConverterContainer.vue
@@ -0,0 +1,117 @@
+<template>
+	<div class="container">
+		<div class="buttons">
+			<button class="button button-left " :class="{ buttonLengthActive : showLength }" @click="showLengthConverter">
+				<img src="../../assets/svg/length.svg" alt="Weight icon">
+			</button>
+			
+			<button class="button button-right " :class="{ buttonWeightActive : showWeight }" @click="showWeightConverter">
+				<img src="../../assets/svg/weight.svg" alt="Weight icon">
+			</button>
+		</div>
+			
+		<LengthConverter v-if="showLength === true"/>
+		<WeightConverter v-if="showWeight === true"/>
+	</div>
+</template>
+
+
+<script>
+	import LengthConverter from '../components/LengthConverter.vue'
+	import WeightConverter from '../components/WeightConverter.vue'
+
+	export default {
+		components: {
+			LengthConverter,
+			WeightConverter,
+		},
+
+		data() {
+			return {
+				showLength: true,
+				showWeight: false,
+			}
+		},
+		
+		methods: {
+			showLengthConverter() {
+				this.showLength = true;
+				this.showWeight = false;
+			},
+			
+			showWeightConverter() {
+				this.showLength = false;
+				this.showWeight = true;
+			}
+		}
+	}
+</script>
+
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		max-width: 40rem;
+		margin: 0 auto;
+		box-shadow: 0.3rem 0.3rem #000;
+		border-radius: 1rem;
+		margin-bottom: 10rem;
+	}
+
+	.buttons {
+		display: flex;
+	}
+
+	.button {
+		background-color: #BEBEBE;
+		height: 6rem;
+		width: 50%;
+		border: none;
+		cursor: pointer;
+		transition: all 0.1s;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.button img {
+		width: 4rem;
+		height: 4rem;
+		transition: all 0.3s;
+		transform: scale(0.8);
+	}
+
+	.button-left {
+		border-left: solid 2px #000;
+		border-top: solid 2px #000;
+		border-right: solid 2px #000;
+		border-bottom: solid 2px #000;
+		z-index: 1;
+		border-radius: 1rem 0 0 0;
+	}
+
+	.button-right {
+		border-right: solid 2px #000;
+		border-top: solid 2px #000;
+		border-bottom: solid 2px #000;
+		border-radius: 0 1rem 0 0;
+	}
+
+	.buttonLengthActive {
+		background-color: #F2D7EE;
+		border-bottom: unset;
+		cursor: unset;
+	} 
+	
+	.buttonWeightActive {
+		background-color: #D9F7FA;
+		border-bottom: unset;
+		cursor: unset;
+	}
+	
+	.buttonLengthActive img,
+	.buttonWeightActive img {
+		transform: scale(1);
+	}
+</style>
